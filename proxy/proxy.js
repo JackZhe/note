7f8a2f605c350bcd07e250b2fdff906e0efbe72f
jinyizhe <jinyizhe@founder.com>
2021-07-14 16:21:22 +0800

    1

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,15 +1,1 @@
-var obj = {};
-Object.defineProperty(obj, "a", {
-  configurable: false,
-  enumerable: false,
-  value: 10,
-  writable: false
-});
-
-var p = new Proxy(obj, {
-  get: function(target, prop) {
-    return 10;
-  }
-});
-
-p.a; //会抛出TypeError
+const p = new Proxy()
