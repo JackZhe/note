a5a02170ddd1af4c82eab5d5c04408ab969c5a2e
jackZhe <1493258264@qq.com>
2021-12-29 22:02:51 +0800

    vue.js defineProperty eventBus 发布订阅模式

---
+++ vue2/eventBus.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ */ 
+
+class Vue {
+  constructor(){
+    // 用来存储事件
+    this.subs = []
+  }
+
+  $on(type, fn) {
+    if(!this.subs[type]){
+      this.subs[type] = []
+    }
+    this.subs[type].push(fn)
+  }
+
+  $emit(type){
+    if(this.subs[type]){
+      const args = Array.prototype.slice.call(arguments, 1)
+      
+      this.subs[type].forEach(fn => {
+        fn(...args)
+      });
+
+    }
+  }
+  
+}
+
+const eventBus = new Vue()
+
+eventBus.$on('sum',function() {
+  let count = [...arguments].reduce((x, y) => x + y)
+  console.log(count)
+})
+eventBus.$emit('sum', 1,2,3)
